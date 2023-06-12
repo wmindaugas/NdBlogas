2db2a39e60e006da8669453205ace25a278094a2
wmindaugas <w.mindaugas@gmail.com>
2023-06-12 10:18:09 +0300

    01 Beginning

--- src/main/java/lt/code/academy/blogasnd/dto/Post.java
+++ src/main/java/lt/code/academy/blogasnd/dto/Post.java
@@ -1,31 +1,31 @@
-//package lt.code.academy.blogasnd.dto;
-//
-//public class Post {
-//    private String title;
-//    private String description;
-//    private String category;
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public void setTitle(String title) {
-//        this.title = title;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public void setDescription(String description) {
-//        this.description = description;
-//    }
-//
-//    public String getCategory() {
-//        return category;
-//    }
-//
-//    public void setCategory(String category) {
-//        this.category = category;
-//    }
-//}
+package lt.code.academy.blogasnd.dto;
+
+public class Post {
+    private String title;
+    private String description;
+    private String category;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
