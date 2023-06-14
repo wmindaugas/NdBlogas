409a05b69bdfde1b80b5eee5bbd7cfe4c6228a33
wmindaugas <w.mindaugas@gmail.com>
2023-06-15 00:47:54 +0300

    02 Problems With Thymeleaf

--- src/main/java/lt/code/academy/blogasnd/controller/BlogPostController.java
+++ src/main/java/lt/code/academy/blogasnd/controller/BlogPostController.java
@@ -1,17 +1,25 @@
 package lt.code.academy.blogasnd.controller;
 
+import lt.code.academy.blogasnd.dto.BlogPost;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/posts")
+@RequestMapping("/blogPosts")
 public class BlogPostController {
 
     @GetMapping("/create")
     public String openCreateBlogPostForm(Model model) {
+        model.addAttribute("blogPost", new BlogPost());
 
-        return "form/post";
+        return "form/blogPost";
+    }
+
+    @PostMapping("/create")
+    public String createPost(BlogPost blogPost) {
+        return "blogPosts";
     }
 }
---
+++ src/main/java/lt/code/academy/blogasnd/dto/BlogPost.java
@@ -0,0 +1,13 @@
+package lt.code.academy.blogasnd.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class BlogPost {
+    private String title;
+    private String description;
+    private String category;
+
+}
